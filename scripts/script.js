8e039536550d7e6536445e66ac893066cddeace0
RiaRiver <riria.river@gmail.com>
2020-08-06 22:30:42 +0300

    lesson 1: task 1

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+let money = 'Деньги';
+let income = 10;
+let addExpenses, deposit, mission;
+let period = 2;
+
+alert('Alert-сообщение для ДЗ Урока №1');
+console.log('Пополнения на счету ' + money + ' за период ' + period + ' месяца: ' + income + '$');
