9c98209b0fb5ed445ad87826f5c34ea6356cbc5c
RiaRiver <riria.river@gmail.com>
2020-08-12 06:23:25 +0300

    feat: make lesson 4 required

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,68 @@
 'use strict';
 
-// Переменные предыдущих уроков
 let money = +prompt('Ваш месячный доход?'),
   income = 'Freelance',
   addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.'),
   deposit = confirm('Есть ли у вас депозит в банке?'),
   mission = 300000,
   period = 9,
-  budgetDay = money / 30,
-  // Переменные текущего урока
+  budgetDay = money / 30, //это из предыдущих уроков, просили не удалять
   expenses1 = prompt('Введите обязательную статью расходов?'),
   amount1 = +prompt('Во сколько это обойдется?'),
   expenses2 = prompt('Введите обязательную статью расходов?'),
   amount2 = +prompt('Во сколько это обойдется?'),
-  budgetMonth,
-  missionPeriod; // Переменная не по заданию, чтобы не дублировать код в выводе
+  missionPeriod, // Переменная не по заданию, чтобы не дублировать код в выводе
+  accumulatedMonth;
 
-// Функционал предыдущих уроков
+// Функции
+const showTypeOf = function (data) {
+  console.log(`Тип значения "${data}": ${typeof data}`);
+};
+
+const getExpensesMonth = function () {
+  return amount1 + amount2;
+};
+
+const getAccumulatedMonth = function () {
+  return money - getExpensesMonth();
+};
+
+const getTargetMonth = function () {
+  return mission / getAccumulatedMonth();
+};
+
+const getStatusIncome = function (budgetDay) {
+  switch (true) {
+    case budgetDay < 0:
+      return 'Что-то пошло не так';
+    case budgetDay < 600:
+      return 'К сожалению, у вас уровень дохода ниже среднего';
+    case budgetDay < 1200:
+      return 'У вас средний уровень дохода';
+    case budgetDay >= 1200:
+      return 'У вас высокий уровень дохода';
+    default:
+      return 'Что-то пошло не так';
+  }
+};
+
+// Инструкции
 addExpenses = addExpenses.toLocaleLowerCase().split(', ');
 
-// Логика
-budgetMonth = money - amount1 - amount2;
-missionPeriod = mission / budgetMonth;
-budgetDay = budgetMonth / 30;
+accumulatedMonth = getAccumulatedMonth();
+budgetDay = accumulatedMonth / 30;
+missionPeriod = getTargetMonth(); // Использую переменную, чтобы в выводе три раза функцию не вызывать
 
-// Вывод в консоль предудущих уроков
-console.log('Type of "money": ' + typeof money);
-console.log('Type of "income": ' + typeof income);
-console.log('Type of "deposit": ' + typeof deposit);
-console.log('Length of string "addExpenses": ' + addExpenses.length);
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-console.log('Array "addExpenses": ', addExpenses);
-// Из предыдущего урока, написано не удалять, но этот вывод не соответствует примеру
-// console.log('budgetDay: ', budgetDay);
-
-//Вывод в консоль текущего урока
-console.log('Бюджет на месяц: ', budgetMonth);
+// Вывод в консоль
+showTypeOf(money);
+showTypeOf(income);
+showTypeOf(deposit);
+console.log('Расходы за месяц: ', getExpensesMonth());
+console.log('Возможные расходы: ', addExpenses);
 console.log(
   `Цель будет достигнута за: ${Math.ceil(missionPeriod)} ${
     missionPeriod <= 1 ? 'месяц' : missionPeriod <= 4 ? 'месяца' : 'месяцев'
   }`
 );
 console.log('Бюджет на день: ', Math.floor(budgetDay));
-
-switch (true) {
-  case budgetDay < 0:
-    console.log('Что-то пошло не так');
-    break;
-  case budgetDay < 600:
-    console.log('К сожалению, у вас уровень дохода ниже среднего');
-    break;
-  case budgetDay < 1200:
-    console.log('У вас средний уровень дохода');
-    break;
-  case budgetDay >= 1200:
-    console.log('У вас высокий уровень дохода');
-    break;
-  default:
-    console.log('Что-то пошло не так');
-}
+console.log('Ваш статус: ', getStatusIncome(budgetDay));
