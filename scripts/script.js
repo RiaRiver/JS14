3cf9207c15e0b857d55b1350e4694c86e6a30eda
RiaRiver <riria.river@gmail.com>
2020-08-14 23:16:38 +0300

    feat: make lesson 7 required

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,4 @@
 'use strict';
-
-let money,
-  income = 'Freelance',
-  addExpenses,
-  deposit,
-  mission = 300000,
-  period = 9,
-  budgetDay = money / 30, //это из предыдущих уроков, просили не удалять
-  expenses = [],
-  expensesAmount,
-  missionPeriod, // Переменная не по заданию, чтобы не дублировать код в выводе
-  accumulatedMonth;
-
 // Функции
 const isNumber = function (v) {
   return !isNaN(parseFloat(v)) && isFinite(v);
@@ -26,71 +13,76 @@ const start = function () {
   return +temp;
 };
 
-const showTypeOf = function (data) {
-  return `Тип значения "${data}": ${typeof data}`;
-};
+// Переменные, объект отдельно, а не через запятую, потому что мне так удобнее читать
+let missionPeriod; // Переменная не по заданию, чтобы не дублировать код в выводе
 
-// Не чистая. Можно разбить на получение статей расходов и сумм в переменные и на суммирование. Надо?
-const getExpensesMonth = function () {
-  let sum = 0;
-  let amount;
+let appData = {
+  budget: 0,
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  expensesMonth: 0,
+  budgetMonth: 0,
+  budgetDay: 0,
+  deposit: false,
+  mission: 300000,
+  period: 9,
+  asking: function () {
+    for (let i = 0; i < 2; i++) {
+      let expense = prompt('Введите обязательную статью расходов?');
+      let amount;
+      do {
+        amount = prompt('Во сколько это обойдется?');
+      } while (!isNumber(amount));
+      appData.expenses[expense] = +amount;
+    }
 
-  for (let i = 0; i < 2; i++) {
-    expenses[i] = prompt('Введите обязательную статью расходов?');
+    appData.addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.')
+      .toLocaleLowerCase()
+      .split(',');
+    appData.addExpenses.forEach((item, i, arr) => (arr[i] = item.trim()));
 
-    do {
-      amount = prompt('Во сколько это обойдется?');
-    } while (!isNumber(amount));
+    appData.deposit = confirm('Есть ли у вас депозит в банке?');
+  },
+  getExpensesMonth: function () {
+    for (let key in appData.expenses) {
+      appData.expensesMonth += appData.expenses[key];
+    }
+  },
 
-    sum += +amount;
-  }
-
-  return sum;
-};
-
-const getAccumulatedMonth = function (money, expensesAmount) {
-  return money - expensesAmount;
-};
-
-const getTargetMonth = function (mission, accumulatedMonth) {
-  return mission / accumulatedMonth;
-};
-
-const getStatusIncome = function (budgetDay) {
-  switch (true) {
-    case budgetDay < 0:
-      return 'Что-то пошло не так';
-    case budgetDay < 600:
-      return 'К сожалению, у вас уровень дохода ниже среднего';
-    case budgetDay < 1200:
-      return 'У вас средний уровень дохода';
-    case budgetDay >= 1200:
-      return 'У вас высокий уровень дохода';
-    default:
-      return 'Что-то пошло не так';
-  }
+  getBudget: function () {
+    appData.budgetMonth = appData.budget - appData.expensesMonth;
+    appData.budgetDay = appData.budgetMonth / 30;
+  },
+  getTargetMonth: function () {
+    return appData.mission / appData.budgetMonth;
+  },
+  getStatusIncome: function () {
+    switch (true) {
+      case appData.budgetDay < 0:
+        return 'Что-то пошло не так';
+      case appData.budgetDay < 600:
+        return 'К сожалению, у вас уровень дохода ниже среднего';
+      case appData.budgetDay < 1200:
+        return 'У вас средний уровень дохода';
+      case appData.budgetDay >= 1200:
+        return 'У вас высокий уровень дохода';
+      default:
+        return 'Что-то пошло не так';
+    }
+  },
 };
 
 // Инструкции
-money = start();
-
-// Пересла сюда ввод из переменных, чтобы данные запрашивались в нужном порядке
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
-deposit = confirm('Есть ли у вас депозит в банке?');
-
-addExpenses = addExpenses.toLocaleLowerCase().split(', ');
-expensesAmount = getExpensesMonth();
-accumulatedMonth = getAccumulatedMonth(money, expensesAmount);
-budgetDay = accumulatedMonth / 30;
-missionPeriod = getTargetMonth(mission, accumulatedMonth); // Использую переменную, чтобы в выводе три раза функцию не вызывать
+appData.budget = start();
+appData.asking();
+appData.getExpensesMonth();
+appData.getBudget();
+missionPeriod = appData.getTargetMonth(); // Использую переменную, чтобы в выводе три раза функцию не вызывать
 
 // Вывод в консоль
-console.log(showTypeOf(money));
-console.log(showTypeOf(income));
-console.log(showTypeOf(deposit));
-
-console.log('Расходы за месяц: ', expensesAmount);
-console.log('Возможные расходы: ', addExpenses);
+console.log('Расходы за месяц: ', appData.expensesMonth);
 console.log(
   missionPeriod < 0
     ? 'Цель не будет достигнута'
@@ -98,5 +90,8 @@ console.log(
         missionPeriod <= 1 ? 'месяц' : missionPeriod <= 4 ? 'месяца' : 'месяцев'
       }`
 );
-console.log('Бюджет на день: ', Math.floor(budgetDay));
-console.log('Ваш статус: ', getStatusIncome(budgetDay));
+console.log('Ваш статус: ', appData.getStatusIncome());
+console.log('Наша программа включает в себя данные:');
+for (let key in appData) {
+  console.log(`${key} : ${appData[key]}`);
+}
