d8ca0ba0278ee9d3da62f5c89346305db8133927
RiaRiver <riria.river@gmail.com>
2020-08-19 20:09:48 +0300

    refactor: simplify getTitle function, refactor getDateString, change logic of output

--- scripts/script.js
+++ scripts/script.js
@@ -11,55 +11,27 @@
 
 // 4) С помощью функции setInterval, реализуйте вывод даты и времени каждую секунду
 
-document.body.style.cssText = 'font-family: sans-serif; color: rgb(255, 0, 0); font-size: 14pt';
+const getTitle = function (value, units) {
+  let lastDigit = value % 10;
+  let last2Digits = value % 100;
+
+  switch (true) {
+    case last2Digits > 10 && last2Digits < 20:
+      return units[2];
+    case lastDigit > 1 && lastDigit < 5:
+      return units[1];
+    case lastDigit === 1:
+      return units[0];
+    default:
+      return units[2];
+  }
+};
+
+const padLeftZero = function (value) {
+  return value < 10 ? '0' + value : value;
+};
 
 const getDateStrings = function (date) {
-  // Функции
-  const getValueText = function (value, units) {
-    const hoursMap = {
-        часа: [2, 3, 4, 22, 23],
-        час: [1, 21],
-      },
-      hoursDefaultUnit = 'часов',
-      minutesMap = {
-        минуты: [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54],
-        минута: [1, 21, 31, 41, 51],
-      },
-      minutesDefaultUnit = 'минут',
-      secondsMap = {
-        секунды: [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54],
-        секунда: [1, 21, 31, 41, 51],
-      },
-      secondsDefaultUnit = 'секунд';
-
-    const getUnitByValue = function (unitsMap) {
-      for (let key in unitsMap) {
-        if (unitsMap[key].includes(value)) {
-          return key;
-        }
-      }
-    };
-
-    switch (units) {
-      case 'hours': {
-        return getUnitByValue(hoursMap) || hoursDefaultUnit;
-      }
-      case 'minutes': {
-        return getUnitByValue(minutesMap) || minutesDefaultUnit;
-      }
-      case 'seconds': {
-        return getUnitByValue(secondsMap) || secondsDefaultUnit;
-      }
-      default:
-        return;
-    }
-  };
-
-  const padLeftZero = function (value) {
-    return value < 10 ? '0' + value : value;
-  };
-
-  // Данные
   const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
     months = [
       'января',
@@ -75,36 +47,51 @@ const getDateStrings = function (date) {
       'ноября',
       'декабря',
     ],
-    day = date.getDate(),
-    month = date.getMonth(),
-    year = date.getFullYear(),
-    hours = date.getHours(),
-    minutes = date.getMinutes(),
-    seconds = date.getSeconds(),
-    dayText = week[date.getDay()],
-    monthText = months[month];
+    currentDate = {
+      day: date.getDate(),
+      month: date.getMonth(),
+      year: date.getFullYear(),
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
+      seconds: date.getSeconds(),
+      dayTitle: week[date.getDay()],
+      monthTitle: '',
+      hoursTitle: '',
+      minutesTitle: '',
+      secondsTitle: '',
+      // Формирование строк
+      stringFormat1: function () {
+        return `Сегодня ${this.dayTitle}, ${this.day} ${this.monthTitle} ${this.year} года, ${this.hours} ${this.hoursTitle} ${this.minutes} ${this.minutesTitle} ${this.seconds} ${this.secondsTitle}`;
+      },
+      stringFormat2: function () {
+        return `${padLeftZero(this.day)}.${padLeftZero(this.month)}.${this.year} - ${padLeftZero(
+          this.hours
+        )}:${padLeftZero(this.minutes)}:${padLeftZero(this.seconds)}`;
+      },
+    };
 
-  const hoursText = getValueText(hours, 'hours'),
-    minutesText = getValueText(minutes, 'minutes'),
-    secondsText = getValueText(seconds, 'seconds');
+  currentDate.monthTitle = months[currentDate.month];
+  currentDate.hoursTitle = getTitle(currentDate.hours, ['час', 'часа', 'часов']);
+  currentDate.minutesTitle = getTitle(currentDate.minutes, ['минута', 'минуты', 'минут']);
+  currentDate.secondsTitle = getTitle(currentDate.seconds, ['секунда', 'секунды', 'секунд']);
 
-  // Формирование строк
-  const dateString1 = `Сегодня ${dayText}, ${day} ${monthText} ${year} года, ${hours} ${hoursText} ${minutes} ${minutesText} ${seconds} ${secondsText}`,
-    dateString2 = `${padLeftZero(day)}.${padLeftZero(month)}.${year} - ${padLeftZero(hours)}:${padLeftZero(
-      minutes
-    )}:${padLeftZero(seconds)}`;
-
-  return [dateString1, dateString2];
+  return [currentDate.stringFormat1(), currentDate.stringFormat2()];
 };
 
+// Подготовка страницы к выводу
+document.body.style.cssText = 'font-family: sans-serif; color: rgb(255, 0, 0); font-size: 14pt';
+
+for (let i = 0; i < 2; i++) {
+  document.body.append(document.createElement('p'));
+}
+
+const paragraphs = document.querySelectorAll('p');
+
+// Функция вывода на экран
 const outputCurrentDateTime = function () {
-  getDateStrings(new Date())
-    .map((item) => {
-      const p = document.createElement('p');
-      p.textContent = item;
-      return p;
-    })
-    .forEach((item) => document.body.append(item));
+  const strings = getDateStrings(new Date());
+  paragraphs.forEach((elem, i) => (elem.textContent = strings[i]));
 };
 
+// Запуск
 window.setInterval(outputCurrentDateTime, 1000);
