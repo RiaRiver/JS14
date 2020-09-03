ca6e11f9063185bea94df7cf9ca7a044eb13a9ba
RiaRiver <riria.river@gmail.com>
2020-09-04 00:47:52 +0300

    feat: make slider according video lesson 21

--- scripts/script.js
+++ scripts/script.js
@@ -46,9 +46,6 @@ const scrollToElem = elem => {
   elem.scrollIntoView({ behavior: 'smooth' });
 };
 
-// TODO[done] Написать 1 обработчик для всех событий внутри функции toggleMenu()
-// TODO[done] Реализовать следующий функционал: если клик произошел мимо меню, оно закрывается
-
 // Меню
 const toggleMenu = (menuButtonSelector, menuSelector) => {
   const menu = document.querySelector(menuSelector);
@@ -183,3 +180,72 @@ const controlTabs = (tabHeader, tabSelector, tabContents) => {
 const tabHeader = document.querySelector('.service-header'),
   tabContents = document.querySelectorAll('.service-tab');
 controlTabs(tabHeader, '.service-header-tab', tabContents);
+
+// TODO[done] Реализовать слайдер на сайте по видеоуроку
+// TODO Удалить все элементы со страницы с классом dot (из верстки Index.html)
+// TODO Написать скрипт, который будет на страницу добавлять точки с классом dot равному количеству слайдов
+
+// Слайдер
+const slider = () => {
+  const slides = document.querySelectorAll('.portfolio-item');
+  const sliderButtons = document.querySelectorAll('.portfolio-btn');
+  const dots = document.querySelectorAll('.dot');
+  const sliderContainer = document.querySelector('.portfolio-content');
+  let currentSlide = 0,
+    interval;
+
+  const prevSlide = (elem, index, strClass) => {
+    elem[index].classList.remove(strClass);
+  };
+  const nextSlide =  (elem, index, strClass)  => {
+    elem[index].classList.add(strClass);
+  };
+  const autoPlaySlide = () => {
+    prevSlide(slides, currentSlide, 'portfolio-item-active');
+    prevSlide(dots, currentSlide, 'dot-active');
+    currentSlide++;
+    if (currentSlide >= slides.length) currentSlide = 0;
+    nextSlide(slides, currentSlide, 'portfolio-item-active');
+    nextSlide(dots, currentSlide, 'dot-active');
+  };
+  const startSlider = (time = 3000) => {
+    interval = setInterval(autoPlaySlide, time);
+  };
+  const stopSlider = () => {
+    clearInterval(interval);
+  };
+
+  sliderContainer.addEventListener('click', event => {
+    event.preventDefault();
+    const target = event.target;
+    if (!target.matches('.portfolio-btn, .dot')) {
+      return;
+    }
+    prevSlide(slides, currentSlide, 'portfolio-item-active');
+    prevSlide(dots, currentSlide, 'dot-active');
+    if (target.matches('#arrow-right')) {
+      currentSlide++;
+    } else if (target.matches('#arrow-left')) {
+      currentSlide--;
+    } else if (target.matches('.dot')) {
+      currentSlide = [...dots].indexOf(target);
+    }
+    if (currentSlide >= slides.length) currentSlide = 0;
+    if (currentSlide < 0) currentSlide = slides.length - 1;
+    nextSlide(slides, currentSlide, 'portfolio-item-active');
+    nextSlide(dots, currentSlide, 'dot-active');
+  });
+
+  sliderContainer.addEventListener('mouseover', event => {
+    if (event.target.matches('.portfolio-btn,.dot ')) {
+      stopSlider();
+    }
+  });
+  sliderContainer.addEventListener('mouseout', event => {
+    if (event.target.matches('.portfolio-btn,.dot ')) {
+      startSlider();
+    }
+  });
+  startSlider(1500);
+};
+slider();
