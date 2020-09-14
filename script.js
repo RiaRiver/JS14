c398b422fe1bc9ba8ccf3b6834e3ccc62d0e3abe
RiaRiver <riria.river@gmail.com>
2020-09-15 02:06:43 +0300

    feat: make lesson 28 complicated

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
-  'use strict';
-
   const select = document.getElementById('cars'),
     output = document.getElementById('output');
 
-  select.addEventListener('change', () => {
+  const getData = () => new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.open('GET', './cars.json');
     request.setRequestHeader('Content-type', 'application/json');
     request.send();
     request.addEventListener('readystatechange', () => {
       if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        data.cars.forEach(item => {
-          if (item.brand === select.value) {
-            const { brand, model, price } = item;
-            output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-          }
-        });
-      } else {
-        output.innerHTML = 'Произошла ошибка';
+        resolve(request.responseText);
+      } else if (request.readyState === 4 && request.status !== 200) {
+        reject();
       }
     });
+
+  });
+
+  const outputData = json => {
+    const data = JSON.parse(json);
+    data.cars.forEach(item => {
+      if (item.brand === select.value) {
+        const { brand, model, price } = item;
+        output.innerHTML = `
+          Тачка ${brand} ${model} <br>
+          Цена: ${price}$
+        `;
+      }
+    });
+  };
+
+  const outputError = () => {
+    output.innerHTML = 'Произошла ошибка';
+  };
+
+  select.addEventListener('change', () => {
+    getData()
+      .then(outputData)
+      .catch(outputError);
   });
 });
